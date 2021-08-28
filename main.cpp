85581c982664e1dfdd033607847ec5d961b3a2d3
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-08-28 02:31:39 -0300

    More Functions

--- main.cpp
+++ main.cpp
@@ -3,18 +3,56 @@
 
 int main()
 {
-    int width = 640;
-    int height = 480;
+    const int width = 640;
+    const int height = 480;
 
     InitWindow(width, height, "Geometry War");
 
+    int circleX = width * 0.2f;
+    int circleY = height / 2;
+
+    int speed = 10;
+    int challengeSpeed = 10;
+    int level = 0;
+
+    int rectPositionY = 0;
+
+    SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+        if (IsKeyDown(KEY_D) && circleX < width)
+        {
+            circleX += speed;
+        }
+        if (IsKeyDown(KEY_A) && circleX > 0)
+        {
+            circleX -= speed;
+        }
+        if (IsKeyDown(KEY_W) && circleY > 0)
+        {
+            circleY -= speed;
+        }
+        if (IsKeyDown(KEY_S) && circleY < height)
+        {
+            circleY += speed;
+        }
+
+        rectPositionY += challengeSpeed;
+
+        // RESET RECT POSITION
+        if (rectPositionY == height)
+        {
+            rectPositionY = 0;
+        }
+
         BeginDrawing();
         ClearBackground(WHITE); //  To avoid Black Flickering
 
-        DrawCircle(width / 2, height / 2, 150, BLUE);
-        DrawCircle(width / 2, height / 2, 100, RED);
+        DrawCircle(circleX, circleY, 25, BLUE);
+
+        DrawRectangle(width / 5 * 4, rectPositionY, 50, 50, RED);
+
+        DrawText(TextFormat("Level: %i", level), 20, 2, 22, BLACK);
 
         EndDrawing();
     }
